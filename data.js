469f8b77de3a3721e9699b18aa49768db68ed529
haolanl363-lhl <haolanl363@gmail.com>
2026-07-25 22:41:23 +0800

    Add investment records data structure

---
+++ data.js
@@ -0,0 +1,13 @@
+// 投资记录数据
+
+let investmentRecords = [
+
+    {
+        stock: "示例股票",
+        buyPrice: 100,
+        currentPrice: 120,
+        quantity: 100,
+        date: "2026-07-25"
+    }
+
+];
